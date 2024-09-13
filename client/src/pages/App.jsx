dc5037a24950b3279e3a6e1144c7292a9e8f9c0f
Sai Sahith <131525241+SaiSahithPolimera@users.noreply.github.com>
2024-09-13 18:33:51 +0530

    feat: add new page for visualization

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
+import SimpleGraph from "./SimpleGraph";
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Visualize" element={<SimpleGraph />} />
       </Routes>
     </>
   );
